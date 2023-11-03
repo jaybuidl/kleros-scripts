4af9e7c51a529ee88610c2cdc08ab7aadad20650
jaybuidl <jb@kleros.io>
2023-11-03 14:48:01 +0000

    fixes

--- scripts/list-netlify-sites.sh
+++ scripts/list-netlify-sites.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-netlify sites:list --json | jq '.[] 
+netlify sites:list --json | jq '[
+  .[] 
+  | select(.account_slug == "kleros")
   | with_entries(select(.key | in({"name":1, "url":1, "deploy_url":1, "published_deploy":1, "build_settings":1}))) 
   | .repo_branch = .build_settings.repo_branch 
+  | .repo_url = .build_settings.repo_url 
   | .allowed_branches = .build_settings.allowed_branches 
   | .available_functions = [.published_deploy.available_functions[]?.n]
   | del(.published_deploy) 
-  | del(.build_settings)'
+  | del(.build_settings)
+]'
