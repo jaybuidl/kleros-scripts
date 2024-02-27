85f2a039b3c6866086670b76e42e62ef6f611c94
jaybuidl <jb@kleros.io>
2024-02-27 20:54:20 +0000

    minor

--- scripts/generate-wallets.ts
+++ scripts/generate-wallets.ts
@@ -6,12 +6,15 @@ import * as QRCode from "qrcode";
 async function main() {
   const COUNT = 50;
   const QRCODE_FOLDER = "./qrcodes";
+  const disperseAmount = 5;
   const wallet = ethers.Wallet.createRandom({ entropy: ethers.utils.randomBytes(32) });
   const words = wallet.mnemonic.phrase;
   const node = ethers.utils.HDNode.fromMnemonic(words);
 
   console.log("MNEMONIC: %s", words);
-  fs.mkdir(join(__dirname, QRCODE_FOLDER), () => {});
+  fs.mkdir(join(__dirname, QRCODE_FOLDER), { recursive: true }, (err) => {
+    if (err) throw err;
+  });
 
   let accounts = "";
   let disperse = "";
@@ -19,7 +22,7 @@ async function main() {
   for (let i = 0; i < COUNT; i++) {
     const account = node.derivePath(`m/44'/60'/0'/0/${i}`);
     accounts += `${account.index},${account.address},${account.privateKey}\n`;
-    disperse += `${account.address},5\n`;
+    disperse += `${account.address},${disperseAmount}\n`;
     await QRCode.toFile(
       join(__dirname, `${QRCODE_FOLDER}/qr-${account.index}-${account.address}.png`),
       account.privateKey
