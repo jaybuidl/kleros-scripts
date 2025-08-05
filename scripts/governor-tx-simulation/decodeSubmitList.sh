e1cf53b2ad55052c8df30bbd6a6ca849917b8648
jaybuidl <jb@kleros.io>
2025-08-06 00:56:40 +0100

    set fork variable, use mesc

--- scripts/governor-tx-simulation/decodeSubmitList.sh
+++ scripts/governor-tx-simulation/decodeSubmitList.sh
@@ -2,7 +2,7 @@
 
 txId=${1:-0xc07ed485c7e4e8dce7e1ee373f6179287cc5d66adc1405a028e990ee9f762924}
 
-input=$(cast tx --rpc-url https://eth-rpc.gateway.pokt.network $txId --json | jq -r .input)
+input=$(cast tx --rpc-url $(mesc url ethereum) $txId --json | jq -r .input)
 functionSig=$(cast 4byte ${input:2:8})
 
 cast --calldata-decode "$functionSig" "$input" 
